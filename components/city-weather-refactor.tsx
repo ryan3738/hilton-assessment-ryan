a332c9122ae9f578b6641572497369eb206f1439
Ryan <ryan3738@gmail.com>
2021-11-09 15:35:38 -0800

    create file, add todos

---
+++ components/city-weather-refactor.tsx
@@ -0,0 +1,6 @@
+// todo: refactor the city-weather component to use function component & react hooks
+// todo: match the design from the design.png file
+// todo: improve accessibility
+    // 1. Ensure that clicking on the label "Weather Search" puts focus into the text-input.
+    // 2. Make sure any loading states are correctly announced to a screen reader
+// todo: create better tests for the component
