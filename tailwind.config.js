7b86eefc42b16ce03ed26ba42469110203587a1e
Ryan <ryan3738@gmail.com>
2021-11-10 15:51:18 -0800

    add custom colors from design file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: "jit",
   purge: ["./**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#4683c8',
+        offwhite: '#e3e8ef'
+      }
+    }
   },
   variants: {
     extend: {}
