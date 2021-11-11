f7290777eba40f9f0a56d307c922db29cf5bc19f
Ryan <ryan3738@gmail.com>
2021-11-10 22:12:57 -0800

    linting

--- components/city-weather-refactor.tsx
+++ components/city-weather-refactor.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import Image from 'next/image'
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 // todo: refactor the city-weather component to use function component & react hooks
 
-
 // to get api key: https://openweathermap.org/appid
-const API_KEY = "b233fc3510574b2cec6129d5d30e9ee2";
+const API_KEY = 'b233fc3510574b2cec6129d5d30e9ee2';
 
 interface CityWeatherProps {
     city: string;
@@ -13,76 +12,87 @@ interface CityWeatherProps {
 interface CityWeatherState {
     main: {
         temp: number;
-    }
+    };
     weather: [
         {
-        description: string;
-        icon: string;
-    }]
+            description: string;
+            icon: string;
+        }
+    ];
     name: string;
     loading: boolean;
 }
 
-export const CityWeather = ({city}:CityWeatherProps) => {
-    const [weatherResult, setWeatherResult] = useState<CityWeatherState | null>(null);
+function KtoF(tempKevlin: number): number {
+    return ((tempKevlin - 273.15) * 9) / 5 + 32;
+}
+
+export const CityWeather = ({ city }: CityWeatherProps) => {
+    const [weatherResult, setWeatherResult] = useState<CityWeatherState | null>(
+        null
+    );
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        const fetchData = async ({city}: {city:string}) => {
-
-            try{
-            setError(null);
-            setWeatherResult(null);
-            setLoading(true);
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-            const response = await fetch(url);
-            if (response.ok) {
-                const data = await response.json();
-                console.log('Weather Data', data);
-                setWeatherResult(data);
-            } else {
-                setError(response.statusText);
-            }
+        const fetchData = async ({ city }: { city: string }) => {
+            try {
+                setError(null);
+                setWeatherResult(null);
+                setLoading(true);
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+                const response = await fetch(url);
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log('Weather Data', data);
+                    setWeatherResult(data);
+                } else {
+                    setError(response.statusText);
+                }
             } finally {
                 setLoading(false);
             }
         };
-        fetchData({city});
+        fetchData({ city });
     }, [city]);
 
     // todo: match the design from the design.png file
     // todo: improve accessibility
 
-
-
     return (
         <>
-            {loading && <div className='w-full h-auto'>Loading...</div>}
-            {error && <div className='w-full h-auto text-red-600'>Error: {error}</div>}
+            {loading && <div className="w-full h-auto">Loading...</div>}
+            {error && (
+                <div className="w-full h-auto text-red-600">Error: {error}</div>
+            )}
             {weatherResult && !loading && !error && (
-            <>
-            <h2 className="text-2xl font-bold text-darkGray uppercase">{weatherResult.name}</h2>
-            <Image
-            src={`http://openweathermap.org/img/wn/${weatherResult?.weather[0].icon}@4x.png`}
-            alt={weatherResult?.weather[0].description}
-            width={100}
-            height={100}
-            ></Image>
-            <div className="text-xl font-semibold text-lightGray capitalize" aria-hidden='true'>{weatherResult?.weather[0].description}</div>
-            <div className="flex flex-row flex-nowrap justify-center items-end my-2">
-                    <h3 className="text-l font-semibold text-lightGray capitalize mr-3">
-                Temperature:</h3>
-                    <span className="text-4xl font-semibold text-gray-800 capitalize">{KtoF(weatherResult?.main.temp).toFixed(0)}&#8457;
-                    </span>
-            </div>
-            </>
-        )}
+                <>
+                    <h2 className="text-2xl font-bold text-darkGray uppercase">
+                        {weatherResult.name}
+                    </h2>
+                    <Image
+                        src={`http://openweathermap.org/img/wn/${weatherResult?.weather[0].icon}@4x.png`}
+                        alt={weatherResult?.weather[0].description}
+                        width={100}
+                        height={100}
+                    />
+                    <div
+                        className="text-xl font-semibold text-lightGray capitalize"
+                        aria-hidden="true"
+                    >
+                        {weatherResult?.weather[0].description}
+                    </div>
+                    <div className="flex flex-row flex-nowrap justify-center items-end my-2">
+                        <h3 className="text-l font-semibold text-lightGray capitalize mr-3">
+                            Temperature:
+                        </h3>
+                        <span className="font-sans text-4xl font-semibold text-gray-800 capitalize">
+                            {KtoF(weatherResult?.main.temp).toFixed(0)}&#8457;
+                        </span>
+                    </div>
+                </>
+            )}
         </>
     );
-}
+};
 // todo: create better tests for the component
-
-function KtoF(tempKevlin: number) {
-    return ((tempKevlin - 273.15) * 9) / 5 + 32;
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { CityWeather } from "../components/city-weather-refactor";
-import Head from 'next/head'
+import { useState } from 'react';
+import Head from 'next/head';
+import { CityWeather } from '../components/city-weather-refactor';
 
 export default function IndexPage() {
   const [city, setCity] = useState<string | null>(null);
@@ -13,41 +13,48 @@ export default function IndexPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Weather App</title>
         <meta name="description" content="A simple weather app" />
-        </Head>
+      </Head>
       <form
         className="flex flex-wrap flex-row items-center justify-center m-6"
-
         onSubmit={(e) => {
           e.preventDefault();
           const formdata = new FormData(e.currentTarget);
-          setCity(formdata.get("city").toString());
+          setCity(formdata.get('city').toString());
         }}
-        >
-
+      >
         <h1>
-        <label  htmlFor="city" className="text-xl font-semibold pr-2 py-2">Weather Search:</label>
+          <label htmlFor="city" className="text-xl font-semibold pr-2 py-2">
+            Weather Search:
+          </label>
         </h1>
         <div className="flex rounded-lg">
-        <input
-          id="city"
-          data-testid="weather-input"
-          title="Enter a city to search for the current weather"
-          className="border border-solid border-primary px-2 py-1 rounded-l-lg h-12 w-48"
-          type="text"
-          name="city"
-        />
-          <button className="text-white text-l uppercase font-semibold rounded-r-lg px-3 py-2 bg-primary" type="submit">
-          Submit
-        </button>
+          <input
+            id="city"
+            data-testid="weather-input"
+            title="Enter a city to search for the current weather"
+            className="border border-solid border-primary px-2 py-1 rounded-l-lg h-12 w-48"
+            type="text"
+            name="city"
+          />
+          <button
+            className="text-white text-l uppercase font-semibold rounded-r-lg px-3 py-2 bg-primary"
+            type="submit"
+          >
+            Submit
+          </button>
         </div>
       </form>
 
       {city && (
-        <section title={`Current forecast`} tabIndex={0} role="status" className=" w-52 h-60 shadow-lg bg-white flex flex-col items-center justify-center rounded-xl text-center  m-8">
-          <CityWeather  city={city}  />
+        <section
+          title="Current forecast"
+          tabIndex={0}
+          role="status"
+          className=" w-52 h-60 shadow-lg bg-white flex flex-col items-center justify-center rounded-xl text-center  m-8"
+        >
+          <CityWeather city={city} />
         </section>
       )}
-
     </main>
   );
 }
